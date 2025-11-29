15a4626f93b345cdf45ff51d13a3382a5d4ed4cd
youngsun3454 <phil.z@wustl.edu>
2025-11-29 17:47:29 -0600

    a version for optimizing

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('dataset.csv')
+
+#pd.fillna()
+
+print(df.describe())
+
+df.isnull.sum()
 
 
 
-print("hello world!")
+
+
+
